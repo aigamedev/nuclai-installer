c13f462657d4d94c34baeb28cb1534b490ef305f
Alex J. Champandard <alexjc@aigamedev.com>
2015-10-08 16:43:43 +0200

    Adding more commands and complete prototype of minecraft terrain installation on OSX.

--- nuclai/main.py
+++ nuclai/main.py
@@ -2,6 +2,7 @@ import os
 import re
 import json
 import subprocess
+import urllib.request
 
 
 class ansi:
@@ -22,17 +23,8 @@ def display(text, color=ansi.WHITE):
     print("%s%s%s" % (color, text, ansi.ENDC))
 
 
-def do_install(package):
-    display('Installing package `%s`.' % (package), ansi.BLUE)
-    pkg = json.load(open(package+'.json'))
-    
-    if not os.path.isdir(package):
-        os.mkdir(package)
-    os.chdir(package)
-
-    log = open('install.log', 'w')
-    def call(*cmdline, cwd=None):
-        subprocess.call(cmdline, stdout=log, stderr=log, cwd=cwd)
+def do_install(name, pkg, call):
+    display('Installing nucl.ai package `%s`.' % (name), ansi.BLUE)
 
     for cmd, *args in pkg['install']['osx']:
         if cmd == 'github':
@@ -45,15 +37,58 @@ def do_install(package):
             if os.path.exists(os.path.join(folder, 'setup.py')):
                 call('python3', 'setup.py', 'develop', cwd=folder)
 
+        if cmd == 'shell':
+            print('  *', style(cmd), args[0])
+            call(*args[1:], shell=True)
+
         if cmd == 'pypi':
             print('  *', style(cmd), *args)
             call('pip', 'install', '--upgrade', *args)
 
+        if cmd == 'exec':
+            if args[0].endswith('.py'):
+                args.insert(0, 'python3')
+            call(*args)
+
+        if cmd == 'fetch':
+            if not os.path.exists(args[1]):
+                print('  *', style(cmd), args[1])
+                urllib.request.urlretrieve(*args)
+
+
+def do_demo(name, pkg, call):
+    display('Demonstrating nucl.ai package `%s`.' % (name), ansi.BLUE)
+    
+    for cmd, *args in pkg['demo']:
+        print('  *', style(cmd), *args)
+
+        if cmd == 'open':
+            call('open', *args)
+
+        if cmd == 'exec':
+            if args[0].endswith('.py'):
+                args.insert(0, 'python3')
+            call(*args)
+
 
 def main(args):
     commands = {
-        'install': do_install
+        'install': do_install,
+        'demo': do_demo
     }
+    mode, package = args[1], args[2]
+    assert mode in commands
+
+    pkg = json.load(open(package+'.json'))
     
-    commands[args[1]](*args[2:])
+    if not os.path.isdir(package):
+        os.mkdir(package)
+    os.chdir(package)
+    
+    log = open(mode+'.log', 'w')
+    def call(*cmdline, **params):
+        ret = subprocess.call(cmdline, stdout=log, stderr=log, **params)
+        if ret != 0: print('Error %i' % ret)
+
+    commands[args[1]](package, pkg, call)
     return 0
