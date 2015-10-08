aa06049c8741ff8575e6f7a283dba0d2b612c5fc
Alex J. Champandard <alexjc@aigamedev.com>
2015-10-08 16:52:13 +0200

    Restructured into an application class to avoid passing around data.

--- nuclai/main.py
+++ nuclai/main.py
@@ -23,72 +23,77 @@ def display(text, color=ansi.WHITE):
     print("%s%s%s" % (color, text, ansi.ENDC))
 
 
-def do_install(name, pkg, call):
-    display('Installing nucl.ai package `%s`.' % (name), ansi.BLUE)
+class Application(object):
 
-    for cmd, *args in pkg['install']['osx']:
-        if cmd == 'github':
-            repo, rev = args
-            folder = re.split('[/.]', repo)[1]
-            print('  *', style(cmd), folder)
-                    
-            call('git', 'clone', 'https://github.com/'+repo)
-            call('git', 'reset', '--hard', rev, cwd=folder)
-            if os.path.exists(os.path.join(folder, 'setup.py')):
-                call('python3', 'setup.py', 'develop', cwd=folder)
-
-        if cmd == 'shell':
-            print('  *', style(cmd), args[0])
-            call(*args[1:], shell=True)
-
-        if cmd == 'pypi':
-            print('  *', style(cmd), *args)
-            call('pip', 'install', '--upgrade', *args)
-
-        if cmd == 'exec':
-            if args[0].endswith('.py'):
-                args.insert(0, 'python3')
-            call(*args)
-
-        if cmd == 'fetch':
-            if not os.path.exists(args[1]):
-                print('  *', style(cmd), args[1])
-                urllib.request.urlretrieve(*args)
-
-
-def do_demo(name, pkg, call):
-    display('Demonstrating nucl.ai package `%s`.' % (name), ansi.BLUE)
+    def call(self, *cmdline, **params):
+        ret = subprocess.call(cmdline, stdout=self.log, stderr=self.log, **params)
+        if ret != 0: print('Error %i' % ret)
     
-    for cmd, *args in pkg['demo']:
-        print('  *', style(cmd), *args)
+    def cmd_install(self, name, pkg):
+        display('Installing nucl.ai package `%s`.' % (name), ansi.BLUE)
+    
+        for cmd, *args in pkg['install']['osx']:
+            if cmd == 'github':
+                repo, rev = args
+                folder = re.split('[/.]', repo)[1]
+                print('  *', style(cmd), folder)
+                        
+                self.call('git', 'clone', 'https://github.com/'+repo)
+                self.call('git', 'reset', '--hard', rev, cwd=folder)
+                if os.path.exists(os.path.join(folder, 'setup.py')):
+                    self.call('python3', 'setup.py', 'develop', cwd=folder)
+    
+            if cmd == 'shell':
+                print('  *', style(cmd), args[0])
+                self.call(*args[1:], shell=True)
+    
+            if cmd == 'pypi':
+                print('  *', style(cmd), *args)
+                self.call('pip', 'install', '--upgrade', *args)
+    
+            if cmd == 'exec':
+                if args[0].endswith('.py'):
+                    args.insert(0, 'python3')
+                self.call(*args)
+    
+            if cmd == 'fetch':
+                if not os.path.exists(args[1]):
+                    print('  *', style(cmd), args[1])
+                    urllib.request.urlretrieve(*args)
 
-        if cmd == 'open':
-            call('open', *args)
-
-        if cmd == 'exec':
-            if args[0].endswith('.py'):
-                args.insert(0, 'python3')
-            call(*args)
+    def cmd_demo(self, name, pkg):
+        display('Demonstrating nucl.ai package `%s`.' % (name), ansi.BLUE)
+        
+        for cmd, *args in pkg['demo']:
+            print('  *', style(cmd), *args)
+    
+            if cmd == 'open':
+                call('open', *args)
+    
+            if cmd == 'exec':
+                if args[0].endswith('.py'):
+                    args.insert(0, 'python3')
+                call(*args)
+    
+    def main(self, args):
+        commands = {
+            'install': self.cmd_install,
+            'demo': self.cmd_demo
+        }
+        cmd, package = args[1], args[2]
+        assert cmd in commands
+    
+        pkg = json.load(open(package+'.json'))
+        
+        if not os.path.isdir(package):
+            os.mkdir(package)
+        os.chdir(package)
+        
+        self.log = open(cmd+'.log', 'w')
+        commands[cmd](package, pkg)
+        return 0
 
 
 def main(args):
-    commands = {
-        'install': do_install,
-        'demo': do_demo
-    }
-    mode, package = args[1], args[2]
-    assert mode in commands
-
-    pkg = json.load(open(package+'.json'))
-    
-    if not os.path.isdir(package):
-        os.mkdir(package)
-    os.chdir(package)
-    
-    log = open(mode+'.log', 'w')
-    def call(*cmdline, **params):
-        ret = subprocess.call(cmdline, stdout=log, stderr=log, **params)
-        if ret != 0: print('Error %i' % ret)
-
-    commands[args[1]](package, pkg, call)
-    return 0
+    app = Application()
+    return app.main(args)
